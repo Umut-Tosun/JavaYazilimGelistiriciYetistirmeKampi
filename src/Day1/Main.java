450f2aeadd215c7da078ec835b5067d82f27923c
Umut Tosun <tosun.umutt@gmail.com>
2022-09-15 11:56:28 +0300

    İlk gün tamamlandı.

---
+++ src/Day1/Main.java
@@ -0,0 +1,25 @@
+package Day1;
+
+public class Main {
+    //main javada başlangıç noktasıdır.
+    public static void main(String[] args) {
+        System.out.println("(2022) Java Yazılım Geliştirici Yetiştirme Kampı 1 : Başlıyoruz!");
+
+        //Değişken isimlendirmeleri java`da camelCase yazılır.
+        String name="Umut";
+        String surName="Tosun";
+        int age=18;
+        double tall=172.5;
+        boolean isStudent=true;
+        String studentNumber="";
+
+        if (isStudent)
+            studentNumber="6776ABC332";
+        else
+            studentNumber="null";
+
+        System.out.println("Ad : "+name+" Soyad : "+surName+" Yaş : "+age+" Boy : "+tall+" Öğrencilik Durumu : "+isStudent+" Öğgrenci No : "+studentNumber);
+
+
+    }
+}
